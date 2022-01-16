9030832ad0ce07ebef9f2daf0f23967fcf4325a6
bharathgangishetti <bharathgangishetti@gmail.com>
2022-01-16 15:43:57 -0500

    title editing

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    path('', views.home, name = 'blog-home'),
+    path('blog/', views.home, name = 'blog-home'),
     path('about/', views.about, name = 'blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author' : 'Bharath',
+        'title' : 'Blog Post 1',
+        'content' : 'First Post Content',
+        'date_posted' : 'January 16 2022'
+    },
+
+    {
+        'author' : 'Gangishetti',
+        'title' : 'Blog Post 2',
+        'content' : 'second Post Content',
+        'date_posted' : 'January 16 2022'
+    }
+]
+
+
+
+
 def home(request):
-    return render(request, 'home.html')
+    context = {
+        'posts' : posts,
+        'title' : 'Blog'
+    }
+    return render(request, 'home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+    return render(request, 'about.html', {'title' : 'about'})
 
 
 # Create your views here.
