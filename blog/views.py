28d08ada7011c189073f1ad3dcd05281e2e3dd60
bharathgangishetti <bharathgangishetti@gmail.com>
2022-01-16 11:15:41 -0500

    AddedHTMLCode

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Bloger Home</h1>')
+    return render(request, 'home.html')
+
+def about(request):
+    return HttpResponse('<h1>Blog about</h1>')
 
 
 # Create your views here.
